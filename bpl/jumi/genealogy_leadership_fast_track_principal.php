b69ce95bb6a020266c72c5d2b315894ab32e0ebe
reyatwel <sysdev@gmx.com>
2025-02-06 06:57:17 +0800

    genealogy lftp

--- bpl/jumi/genealogy_leadership_fast_track_principal.php
+++ bpl/jumi/genealogy_leadership_fast_track_principal.php
@@ -2,17 +2,23 @@
 
 namespace BPL\Jumi\Genealogy_Leadership_Fast_Track_Principal;
 
+require_once 'templates/sb_admin/tmpl/master.tmpl.php';
 require_once 'bpl/ajax/ajaxer/genealogy.php';
 require_once 'bpl/mods/helpers.php';
+
+use function Templates\SB_Admin\Tmpl\Master\main as master;
 
 use function BPL\Ajax\Ajaxer\Genealogy\main as genealogy;
 
 use function BPL\Mods\Helpers\session_get;
 use function BPL\Mods\Helpers\page_validate;
-use function BPL\Mods\Helpers\menu;
+// use function BPL\Mods\Helpers\menu;
 use function BPL\Mods\Helpers\input_get;
+// use function BPL\Mods\Helpers\live_reload;
 
-main();
+$content = main();
+
+master($content);
 
 /**
  *
@@ -25,16 +31,48 @@ function main()
 
 	page_validate();
 
-	$str = menu();
+	// $str = menu();
 
 	$uid = input_get('uid');
+
+	// $str = live_reload(true);
 
 	if ($uid !== '') {
 		$user_id = $uid;
 	}
 
-	$str .= '<h1>Genealogy Tree</h1>';
-	$str .= genealogy('indirect', $user_id, 'leadership_fast_track_principal');
+	$view_structure = view_structure($user_id);
 
-	echo $str;
+	$str = <<<HTML
+	<div class="container-fluid px-4">
+		<h1 class="mt-4">Genealogy Tree</h1>
+		<ol class="breadcrumb mb-4">
+			<li class="breadcrumb-item active">Structure of Accounts</li>
+		</ol>				
+		$view_structure
+	</div>
+	HTML;
+
+	// $str .= '<h1>Genealogy Tree</h1>';
+	// $str .= genealogy('indirect', $user_id, 'leadership_fast_track_principal');
+
+	return $str;
+}
+
+function view_structure($user_id)
+{
+	$view_structure = genealogy('indirect', $user_id, 'leadership_fast_track_principal');
+
+	return <<<HTML
+	<div class="card mb-4">
+		<div class="card-header">
+			<i class="fas fa-sitemap me-1"></i>
+			Royalty Bonus
+		</div>
+		<div class="card-body">
+		$view_structure
+		</div>
+		<div class="card-footer small text-muted">Genealogy Structure</div>
+	</div>
+	HTML;
 }
